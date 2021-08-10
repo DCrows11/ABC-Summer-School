22f001e0104f5c509cf0b7e16dcb2a3c8255d138
Letu-Andrei <letuandrei@yahoo.com>
2021-08-10 15:00:03 +0300

    Homepage design

--- wp-content/themes/Bootstrap-on-WordPress-master/page-custom-homepage.php
+++ wp-content/themes/Bootstrap-on-WordPress-master/page-custom-homepage.php
@@ -3,6 +3,11 @@ if (!is_user_logged_in()) {
     wp_redirect('http://localhost/login');
     exit;
 }
+
+function style_define()
+{
+    return 'style-custom-homepage';
+}
 ?>
 
 <?php BsWp::get_template_parts(array(
@@ -11,9 +16,9 @@ if (!is_user_logged_in()) {
 )); ?>
 
 <div class="homepage-shop-redirect-buttons">
-    <a href="localhost/weapons"><button class="homepage-weapons-redirect-button">Weapons</button></a>
-    <a href="localhost/ammo"><button class="homepage-ammo-redirect-button">Ammo</button></a>
-    <a href="localhost/accessories"><button class="homepage-accessories-redirect-button">Accessories</button></a>
+    <a href="localhost/weapons"><button class="homepage-weapons-redirect-button homepage-button"></button></a>
+    <a href="localhost/ammo"><button class="homepage-ammo-redirect-button homepage-button"></button></a>
+    <a href="localhost/accessories"><button class="homepage-accessories-redirect-button homepage-button"></button></a>
 </div>
 
 <?php BsWp::get_template_parts(array(
